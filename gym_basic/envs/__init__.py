6235c9ef8032019e58d6e1a3184ef1f5ce49da84
Thachanon DK <47318864+thachanon27@users.noreply.github.com>
2022-03-28 14:40:28 +0700

    Update __init__.py

--- gym_basic/envs/__init__.py
+++ gym_basic/envs/__init__.py
@@ -1,3 +1,4 @@
 from gym_basic.envs.basic_env import BasicEnv
 from gym_basic.envs.basic_env_2 import BasicEnv2
 from gym_basic.envs.dog_env import DogEnv
+from gym_basic.envs.Robot_in_room import RobotInRoom
